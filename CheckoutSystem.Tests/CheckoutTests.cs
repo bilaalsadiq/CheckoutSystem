01506817c78728f473c9b24d6cab42bc63c32aba
Bilaal Ahmad <32025068+bilaalsadiq@users.noreply.github.com>
2024-04-28 14:18:32 +0100

    - created test class for testing the Checkout class in the class library layer. - wrote 'Red' Tests for Scanning 'Single Items'.

---
+++ CheckoutSystem.Tests/CheckoutTests.cs
@@ -0,0 +1,56 @@
+using CheckoutSystem;
+
+namespace CheckoutSystem.Tests
+{
+    public class CheckoutTests
+    {
+        [Fact]
+        public void Test_ScanSingleItem_ItemA()
+        {
+            //arrange
+            var checkout = new Checkout();
+
+            //act
+            checkout.Scan("A");
+
+            //assert
+            Assert.Equal(50, checkout.GetTotalPrice());
+        }
+        [Fact]
+        public void Test_ScanSingleItem_ItemB()
+        {
+            //arrange
+            var checkout = new Checkout();
+
+            //act
+            checkout.Scan("B");
+
+            //assert
+            Assert.Equal(30, checkout.GetTotalPrice());
+        }
+        [Fact]
+        public void Test_ScanSingleItem_ItemC()
+        {
+            //arrange
+            var checkout = new Checkout();
+
+            //act
+            checkout.Scan("C");
+
+            //assert
+            Assert.Equal(20, checkout.GetTotalPrice());
+        }
+        [Fact]
+        public void Test_ScanSingleItem_ItemD()
+        {
+            //arrange
+            var checkout = new Checkout();
+
+            //act
+            checkout.Scan("D");
+
+            //assert
+            Assert.Equal(15, checkout.GetTotalPrice());
+        }
+    }
+}
