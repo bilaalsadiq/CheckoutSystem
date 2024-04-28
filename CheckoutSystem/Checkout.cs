ff30dce878c9414ee75b0ef1774537f0d51d70b7
Bilaal Ahmad <32025068+bilaalsadiq@users.noreply.github.com>
2024-04-28 14:04:47 +0100

    Add project files.

---
+++ CheckoutSystem/Checkout.cs
@@ -0,0 +1,15 @@
+﻿namespace CheckoutSystem
+{
+    public class Checkout : ICheckout
+    {
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(string Item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CheckoutSystem/ICheckout.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutSystem
+{
+    internal interface ICheckout
+    {
+        void Scan(string Item);
+        int GetTotalPrice();
+    }
+}
